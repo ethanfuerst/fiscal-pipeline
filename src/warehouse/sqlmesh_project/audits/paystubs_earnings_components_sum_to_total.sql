ff8436f04c4128948e9221d534d0785099f3fc39
Ethan Fuerst <34084535+ethanfuerst@users.noreply.github.com>
2026-05-03 09:35:24 -0400

    Adding fitness benefit category (#32)

--- src/warehouse/sqlmesh_project/audits/paystubs_earnings_components_sum_to_total.sql
+++ src/warehouse/sqlmesh_project/audits/paystubs_earnings_components_sum_to_total.sql
@@ -8,18 +8,20 @@ SELECT
   earnings_salary_usd,
   earnings_bonus_usd,
   earnings_meal_allowance_usd,
+  earnings_fitness_benefit_usd,
   earnings_pto_payout_usd,
   earnings_severance_usd,
   earnings_misc_usd,
   earnings_expense_reimbursement_usd,
   earnings_nyc_citi_bike_usd,
-  earnings_salary_usd + earnings_bonus_usd + earnings_meal_allowance_usd + earnings_pto_payout_usd + earnings_severance_usd + earnings_misc_usd + earnings_expense_reimbursement_usd + earnings_nyc_citi_bike_usd AS calculated_total,
+  earnings_salary_usd + earnings_bonus_usd + earnings_meal_allowance_usd + earnings_fitness_benefit_usd + earnings_pto_payout_usd + earnings_severance_usd + earnings_misc_usd + earnings_expense_reimbursement_usd + earnings_nyc_citi_bike_usd AS calculated_total,
   earnings_total_usd
 FROM @this_model
 WHERE NOT (
   earnings_salary_usd
   + earnings_bonus_usd
   + earnings_meal_allowance_usd
+  + earnings_fitness_benefit_usd
   + earnings_pto_payout_usd
   + earnings_severance_usd
   + earnings_misc_usd
